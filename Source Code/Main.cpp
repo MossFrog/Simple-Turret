b6271abeb603cf89d14946a792bae55fa7d96284
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-07-28 00:23:45 +0200

    Updated the enemy balance and added primary game foundations such as health

--- Source Code/Main.cpp
+++ Source Code/Main.cpp
@@ -18,7 +18,7 @@ int main()
 
 	//-- The variable is defined with its diameter --//
 	sf::CircleShape turretBase(36);
-	turretBase.setOrigin(36,36);
+	turretBase.setOrigin(36, 36);
 	turretBase.setFillColor(sf::Color::Blue);
 	turretBase.setPosition(512, 350);
 
@@ -26,7 +26,7 @@ int main()
 	barrel.setOrigin(0, 6);
 	barrel.setSize(sf::Vector2f(100, 12));
 	barrel.setFillColor(sf::Color::Cyan);
-	barrel.setPosition(512,350);
+	barrel.setPosition(512, 350);
 
 	int barrelAngle = 0;
 
@@ -65,7 +65,7 @@ int main()
 	sf::Sprite barrelSprite;
 	barrelSprite.setTexture(barrelTexure);
 	barrelSprite.setScale(2, 2);
-	barrelSprite.setOrigin(17,41);
+	barrelSprite.setOrigin(17, 41);
 	barrelSprite.setPosition(barrel.getPosition());
 
 	sf::Texture backGroundTexture;
@@ -73,8 +73,39 @@ int main()
 
 	sf::Sprite backGroundSprite;
 	backGroundSprite.setTexture(backGroundTexture);
-	backGroundSprite.setScale(1.43, 1.4);
+	backGroundSprite.setScale(1.24, 1.19);
 	backGroundSprite.setPosition(-1, -1);
+
+	sf::Font mainFont;
+	if (!mainFont.loadFromFile("monumentA.ttf"))
+	{
+	}
+	else
+	{
+		cout << "Loaded 'monumentA.ttf'" << endl;
+	}
+
+	sf::Text startText;
+	sf::Text titleText;
+	sf::Text highScoresText;
+
+	startText.setFont(mainFont);
+	titleText.setFont(mainFont);
+	highScoresText.setFont(mainFont);
+
+	titleText.setString("Simple Turret");
+	titleText.setCharacterSize(80);
+	titleText.setPosition(200, 150);
+
+	startText.setFont(mainFont);
+	startText.setCharacterSize(60);
+	startText.setString("Start");
+	startText.setPosition(400, 250);
+
+	highScoresText.setFont(mainFont);
+	highScoresText.setCharacterSize(60);
+	highScoresText.setString("Highscores");
+	highScoresText.setPosition(300, 350);
 
 
 	//--------------------------//
@@ -120,7 +151,7 @@ int main()
 		{
 			//-- If the mouse button is pressed and the turret timer is ready add a new bullet to the projectile Vector --//
 
-			if (fireClock.getElapsedTime().asMilliseconds() >= 300-fireRate+100)
+			if ((fireClock.getElapsedTime().asMilliseconds() >= 300 - fireRate + 100) && gameState == 1)
 			{
 				projectile newBullet;
 				newBullet.currentPos = turretBase.getPosition();
@@ -148,57 +179,60 @@ int main()
 		//-- Update all the bullet positions --//
 		for (int i = 0; i < projectileVector.size(); i++)
 		{
-			projectileVector[i].currentPos = sf::Vector2f(projectileVector[i].currentPos.x + cos(projectileVector[i].angle*(0.01750))*projectileVector[i].velocity,
-				projectileVector[i].currentPos.y + sin(projectileVector[i].angle*(0.01750))*projectileVector[i].velocity);
-
-			projectileVector[i].render.setPosition(projectileVector[i].currentPos);
-
-			//-- If the bullet position is out of the bounds of the render window then delete it --//
-			if (projectileVector[i].currentPos.x > 1024 || projectileVector[i].currentPos.x < 0)
+			if (gameState == 1)
 			{
-				projectileVector.erase(projectileVector.begin() + i);
-			}
 
-			else if (projectileVector[i].currentPos.y > 700 || projectileVector[i].currentPos.y < 0)
-			{
-				projectileVector.erase(projectileVector.begin() + i);
-			}
+				projectileVector[i].currentPos = sf::Vector2f(projectileVector[i].currentPos.x + cos(projectileVector[i].angle*(0.01750))*projectileVector[i].velocity,
+					projectileVector[i].currentPos.y + sin(projectileVector[i].angle*(0.01750))*projectileVector[i].velocity);
 
-			//-- Do a collision test with all enemies --//
-			for (int j = 0; j < enemyVector.size(); j++)
-			{
-				double tempDistance = sqrt(pow((enemyVector[j].currentPos.x - projectileVector[i].currentPos.x), 2) + pow((enemyVector[j].currentPos.y - projectileVector[i].currentPos.y), 2));
-				if (tempDistance < 15)
+				projectileVector[i].render.setPosition(projectileVector[i].currentPos);
+
+				//-- If the bullet position is out of the bounds of the render window then delete it --//
+				if (projectileVector[i].currentPos.x > 1024 || projectileVector[i].currentPos.x < 0)
 				{
-					//-- If a collision is detected remove the projectile and damage the enemy --//
-					//-- If the enemy health is zero or lower then also remove the enemy --//
-					enemyVector[j].health -= 1;
-
-					//-- Additional "Slowing" Effect --//
-					enemyVector[j].speed = enemyVector[j].speed * 0.5;
-
 					projectileVector.erase(projectileVector.begin() + i);
+				}
 
-					if (enemyVector[j].health <= 0)
+				else if (projectileVector[i].currentPos.y > 700 || projectileVector[i].currentPos.y < 0)
+				{
+					projectileVector.erase(projectileVector.begin() + i);
+				}
+
+				//-- Do a collision test with all enemies --//
+				for (int j = 0; j < enemyVector.size(); j++)
+				{
+					double tempDistance = sqrt(pow((enemyVector[j].currentPos.x - projectileVector[i].currentPos.x), 2) + pow((enemyVector[j].currentPos.y - projectileVector[i].currentPos.y), 2));
+					if (tempDistance < 15)
 					{
-						enemyVector.erase(enemyVector.begin() + j);
-						enemiesKilled += 1;
+						//-- If a collision is detected remove the projectile and damage the enemy --//
+						//-- If the enemy health is zero or lower then also remove the enemy --//
+						enemyVector[j].health -= 1;
+
+						//-- Additional "Slowing" Effect --//
+						enemyVector[j].speed = enemyVector[j].speed * 0.5;
+
+						projectileVector.erase(projectileVector.begin() + i);
+
+						if (enemyVector[j].health <= 0)
+						{
+							enemyVector.erase(enemyVector.begin() + j);
+							enemiesKilled += 1;
+						}
 					}
 				}
 			}
-
 		}
 
 
 		//-- Spawn a new enemy --//
-		if (enemySpawnClock.getElapsedTime().asMilliseconds() >= (1500 - (enemiesKilled*3)))
+		if ((enemySpawnClock.getElapsedTime().asMilliseconds() >= (1500 - (enemiesKilled * 3))) && gameState == 1)
 		{
 			enemy newEnemy;
 
 			newEnemy.currentPos = randomSpawnPos();
 			newEnemy.render.setPosition(newEnemy.currentPos);
 			newEnemy.render.setRadius(15);
-			
+
 			randomEnemyType(newEnemy);
 
 			newEnemy.render.setOrigin(15, 15);
@@ -213,29 +247,32 @@ int main()
 			enemySpawnClock.restart();
 		}
 
+		//-- Enemy update loop --//
 		for (int i = 0; i < enemyVector.size(); i++)
 		{
-			double currentDistance = sqrt(pow((enemyVector[i].currentPos.x - turretBase.getPosition().x), 2) + pow((enemyVector[i].currentPos.y - turretBase.getPosition().y), 2));
-			if (currentDistance > 50)
+			if (gameState == 1)
 			{
-				enemyVector[i].currentPos = enemyVector[i].currentPos - enemyVector[i].direction;
-			}
-
-			else if (currentDistance <= 50)
-			{
-				//-- In this case the enemy has reached the turret base, damage the player and remove the enemy --//
-				enemyVector.erase(enemyVector.begin() + i);
-				playerHealth -= 5;
-
-				if (playerHealth <= 0)
+				double currentDistance = sqrt(pow((enemyVector[i].currentPos.x - turretBase.getPosition().x), 2) + pow((enemyVector[i].currentPos.y - turretBase.getPosition().y), 2));
+				if (currentDistance > 50)
 				{
-					//-- Game over State --//
-					playerHealth = 0;
+					enemyVector[i].currentPos = enemyVector[i].currentPos - enemyVector[i].direction;
 				}
+
+				else if (currentDistance <= 50)
+				{
+					//-- In this case the enemy has reached the turret base, damage the player and remove the enemy --//
+					enemyVector.erase(enemyVector.begin() + i);
+					playerHealth -= 5;
+
+					if (playerHealth <= 0)
+					{
+						//-- Game over State --//
+						playerHealth = 0;
+					}
+				}
+
+				enemyVector[i].render.setPosition(enemyVector[i].currentPos);
 			}
-
-			enemyVector[i].render.setPosition(enemyVector[i].currentPos);
-
 		}
 
 		barrelSprite.setRotation(barrel.getRotation() + 90);
@@ -244,22 +281,33 @@ int main()
 
 		mainWindow.draw(backGroundSprite);
 
-		mainWindow.draw(turretBase);
-		for (int i = 0; i < projectileVector.size(); i++)
+		if (gameState == 1)
 		{
-			mainWindow.draw(projectileVector[i].render);
+			mainWindow.draw(turretBase);
+			for (int i = 0; i < projectileVector.size(); i++)
+			{
+				mainWindow.draw(projectileVector[i].render);
+			}
+
+			for (int i = 0; i < enemyVector.size(); i++)
+			{
+				mainWindow.draw(enemyVector[i].render);
+			}
+
+
+			//-- Uncomment this section to render the barrel entity --//
+			//mainWindow.draw(barrel);
+
+			mainWindow.draw(barrelSprite);
 		}
 
-		for (int i = 0; i < enemyVector.size(); i++)
+		else if (gameState == 0)
 		{
-			mainWindow.draw(enemyVector[i].render);
+			mainWindow.draw(titleText);
+			mainWindow.draw(startText);
+			mainWindow.draw(highScoresText);
 		}
 
-		//-- Uncomment this section to render the barrel entity --//
-		//mainWindow.draw(barrel);
-
-		mainWindow.draw(barrelSprite);
-		
 		mainWindow.display();
 
 	}
